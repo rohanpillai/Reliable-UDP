afda11683ff80494d9b28f6c137aebb77a770f50
Rohan Pillai <rpillai@indiana.edu>
2015-03-23 20:00:00 -0400

    Work in progressw

--- connection.h
+++ connection.h
@@ -16,6 +16,11 @@
 #include <reliableUDP.h>
 
 #define MAX_INET INET6_ADDRSTRLEN
+
+struct session_reliable_udp {
+  int ISN, sequence_number, acknowledged_number, last_acknowledged;
+  bool ack, rst, fin, syn;
+};
  
 int initiate_connection(char *, char *, bool);
 char* get_ip_str(struct sockaddr *, char *, size_t);
